19ac5a9ea211393276f8fdbf4f808d7e2b2e3b71
marepifanio <marcelog.epifanio@gmail.com>
2022-02-16 15:53:42 -0300

    DTO, repository and controller classes/interface created

---
+++ src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
@@ -0,0 +1,34 @@
+package one.digitalinnovation.personapi.controller;
+
+import one.digitalinnovation.personapi.dto.MessageResponseDTO;
+import one.digitalinnovation.personapi.entity.Person;
+import one.digitalinnovation.personapi.repository.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/people")
+public class PersonController {
+
+    private PersonRepository personRepository;
+
+    @Autowired
+    public PersonController (PersonRepository personRepository){
+        this.personRepository = personRepository;
+    }
+
+    @GetMapping
+    public String getMessage(){
+        return "API Test - OK!";
+    }
+
+    @PostMapping
+    public MessageResponseDTO createPerson(@RequestBody Person person){
+        Person savedPerson = personRepository.save(person);
+        return MessageResponseDTO
+                .builder()
+                .massage("Saved person with ID = " + savedPerson.getId())
+                .build();
+    }
+
+}
